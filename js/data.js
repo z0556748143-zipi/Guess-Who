e04883a597c652b040b116c880448ceef8b116dc
zipiL <y0506282605@gmail.com>
2026-04-07 00:52:05 +0300

    add array of questions

--- js/data.js
+++ js/data.js
@@ -17,3 +17,17 @@ export const peopleToGuess = [
 
 let num= Math.floor(Math.random() * peopleToGuess.length);
 export const personToGuess=peopleToGuess[num];
+
+
+
+export const questions = [
+    { id: 1, text: "Is it a man? 👨", property: "isMan" },
+    { id: 2, text: "Is it a baby? 👶", property: "isBaby" },
+    { id: 3, text: "Does he have freckles? ✨", property: "hasFreckles" },
+    { id: 4, text: "Is there a hat on the head? 🎩", property: "hasHat" },
+    { id: 5, text: "Is he holding something? 🎁", property: "isHoldingSomething" },
+    { id: 6, text: "Is there a hair decoration? 🎀", property: "hasHairDecoration" },
+    { id: 7, text: "Is he bearded? 🧔", property: "isBearded" },
+    { id: 8, text: "Is the hair curly? 👨‍🦱", property: "isCurlyHair" },
+    { id: 9, text: "Does he wear glasses? 👓", property: "hasGlasses" }
+];
